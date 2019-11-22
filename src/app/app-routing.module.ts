2fe767516981ffdb8799a68345eb87eee8b60621
unknown <nhussaini@luc.edu>
2019-11-22 15:33:35 -0800

    added the routes(router-outlet)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServiceComponent } from './components/service/service.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'service', component: ServiceComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
